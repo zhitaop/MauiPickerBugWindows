a51499dc1840ab11e46b09cec97290451a3e9554
Zhitao Pan <zpan@esri.com>
2023-10-24 11:54:46 +1100

    Initial commit

---
+++ MauiPickerBugWindows/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiPickerBugWindows
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
